ba702ccedc53ccd7255fd24f23425550a39c0cc9
PAVF <pnvillavicencio@uc.cl>
2023-11-01 21:19:21 -0300

    Added Single Friction Pendulum Bearing Element

--- PythonModels/Single_Friction_Pendulum_Bearing_Element.py
+++ PythonModels/Single_Friction_Pendulum_Bearing_Element.py
@@ -29,3 +29,15 @@ g=9.807*U.m/U.sec**2
 P=80*U.kN
 m=P/g*U.kg
 
+# Geometry
+ops.node(1,0.0,0.0)
+ops.node(2,0.0,0.0)
+
+# Applying kinematic boundary condition
+ops.fix(1,1,1,1);
+ops.fix(2,0,0,1);
+
+# Define material models
+
+
+
